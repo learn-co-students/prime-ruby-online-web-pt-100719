1d0dec14dca7d5efe21248ed0c1335c1deb49fdf
David Bermudez <juan.bermudez1102@outlook.com>
2019-10-09 15:00:18 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+def prime?(number)
+  number = number.abs
+    temp_array = (0..number).to_a - [0]
+    counter = 0
+    temp_array.each { |e|
+      if(number%e == 0)
+        counter += 1
+      end
+    }
+    if(counter == 2)
+      true
+    else
+      false
+    end
+end
