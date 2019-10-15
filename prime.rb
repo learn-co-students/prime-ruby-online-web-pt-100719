35e3be69b229ed549850c3eb3d50a9e946e57add
laffitte.digital@gmail.com <laffitte.digital@gmail.com>
2019-10-14 20:30:47 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime?(int)
+	if int > 1
+		test_range = (2...int).to_a
+  	test_range.each do |elem|
+			if int % elem == 0
+				return false
+			end
+		end
+	return true
+	elsif int <= 1
+		return false
+	end
+end 
+
