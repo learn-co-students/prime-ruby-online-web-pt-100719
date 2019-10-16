805a0cdfed71b44a045df327a1034317fae24128
Jayson Kesterson <jayson.kesterson24@gmail.com>
2019-10-16 14:00:55 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
 # Add  code here!
+def prime?(number)
+  
+  if number < 2 || number == 4
+    return false
+  end
+  
+  count = 2
+  highnum = number / count
+  range = []
+  boolean = nil
+  
+  while count < highnum do
+    if number % count == 0
+      return false
+    end
+    count += 1
+  end
+  return true
+end
