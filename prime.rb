f5010d12ec715a19fa89073f88943344a1334119
Justin Miller <jayman1294@gmail.com>
2019-10-17 06:28:46 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
+
 # Add  code here!
+def prime?(num)
+
+  new_array = (2..num -1).to_a  
+    if num <= 1 
+      return false 
+    end
+  new_array.each do |n|
+    if num % n  == 0 
+      return  false 
+    end
+  end
+ true
+end
