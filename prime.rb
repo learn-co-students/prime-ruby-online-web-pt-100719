ca502961f1389011f3ccdd379a1945012831d6c7
github username <github email address>
2019-10-16 23:16:50 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(num)
+    divisor = 2 #starts dividing with 2
+    if num > 1 
+        try_each = (divisor..num-1).to_a 
+        #creates an array from 2 to one less than num and divides num by each 
+        try_each.none? do |final_num|
+            num % final_num == 0
+        end
+    else
+        false
+    end
+end
+
