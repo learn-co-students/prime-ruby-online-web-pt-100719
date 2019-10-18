7e0a0a79afeb4d2b1a463005bc6e8ce7d80a5dbf
Santiago Salazar <santiago.s.pavajeau@gmail.com>
2019-10-18 21:28:37 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+def prime?(n)
+  if n<2  
+    return false
+  else 
+    (2..n-1).each do |d| 
+      if n % d == 0
+      return false
+      end
+    end
+  end
+  return true
+end
