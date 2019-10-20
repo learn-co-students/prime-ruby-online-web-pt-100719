83e5cf09256b3f0783d3d3f825b185336fcdc641
twwright <twwright@gmail.com>
2019-10-20 16:17:20 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(num)
+    if num <= 1
+        return false
+    else
+        divisors=*(1..num)
+        modulos = []
+        divisors.each {|i| modulos << num % i }
+        if modulos.any? {|i| i == 0 && i != 1 && i != num}
+            return false
+        else   
+            return true
+        
+    end
+end
